0e3959e8cfbb92b8e7c487fe9e72812bdb9a6d89
clcamou <clcamou@gmail.com>
2020-01-12 19:32:19 -0700

    added node modules and JSON

--- index.js
+++ index.js
@@ -1,11 +1,36 @@
-//const questions = [
-  
-//];
+//nodes
+const fs = require("fs");
 
-//function writeToFile(fileName, data) {
+const inquirer = require("inquirer");
+
+const axios = require("axios");
+
+const convertFactory = require("electron-html-to");
+
+const generate = require("./generateHTML.js");
+
+let results; 
+
+//question objects to find the username and favorite color of the user
+const questions = [
+    {
+        type: "input",
+        message: "What is your GitHub username?",
+        name: "username",
+    },
+    {
+        type: "list",
+        name: "colors",
+        message: "What is your favorite color?",
+        choices: ["green", "blue", "red", "pink"],
+    },
+];
+
+//Write the users data to the profile 
+function writeToFile(fileName, data) {
  
-//}
+};
 
-//function init() {
+function init() {}
 
-//init();
+init();
