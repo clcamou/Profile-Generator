eb03dbfa4a6df782b1d1d5e7adb846315629ae63
clcamou <clcamou@gmail.com>
2020-01-18 12:57:58 -0700

    cleaned up the JavaScript

--- index.js
+++ index.js
@@ -11,32 +11,7 @@ let generateHTML = require("./generateHTML.js");
 
 let newHTML;
 let color;
-const colors = {
-    green: {
-      wrapperBackground: "#E6E1C3",
-      headerBackground: "#C1C72C",
-      headerColor: "black",
-      photoBorderColor: "#black"
-    },
-    blue: {
-      wrapperBackground: "#5F64D3",
-      headerBackground: "#26175A",
-      headerColor: "white",
-      photoBorderColor: "#73448C"
-    },
-    pink: {
-      wrapperBackground: "#879CDF",
-      headerBackground: "#FF8374",
-      headerColor: "white",
-      photoBorderColor: "#FEE24C"
-    },
-    red: {
-      wrapperBackground: "#DE9967",
-      headerBackground: "#870603",
-      headerColor: "white",
-      photoBorderColor: "white"
-    }
-  };
+
 
 //question objects to find the username and favorite color of the user
 function promptUser(){
